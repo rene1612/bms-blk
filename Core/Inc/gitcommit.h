67e044c6629aab150a88be6a3ea62a8c7d119efc
Rene Schönrock <rene.schoenrock@online.de>
2024-12-04 21:37:51 +0100

    git commit handling für softwareinformationen

---
+++ Core/Inc/gitcommit.h
@@ -0,0 +1,5 @@
+#define GIT_INFO_PRESENT
+#define __GIT_SHORT_HASH__ 0x7fc004e
+#define __GIT_BRANCH__ "HEAD -> neey_dev, tag: dev_1.0.1, github/neey_dev"
+#define __GIT_DATE_STR__ "2024-11-25"
+#define __GIT_DATE_UT__ 1732571756
