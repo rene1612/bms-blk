760e8fdec5d67e7bb70ad2de3567bbf8fb8b576c
René Schönrock <rene.schoenrock@ifak.eu>
2024-12-12 19:51:32 +0100

    temperatursensoren hinzugefügt

--- Core/Inc/gitcommit.h
+++ Core/Inc/gitcommit.h
@@ -1,5 +1,5 @@
 #define GIT_INFO_PRESENT
-#define __GIT_SHORT_HASH__ 0x9002582
-#define __GIT_BRANCH__ "HEAD -> neey_dev, github/neey_dev"
+#define __GIT_SHORT_HASH__ 0xa904041
+#define __GIT_BRANCH__ "HEAD -> neey_dev, origin/neey_dev"
 #define __GIT_DATE_STR__ "2024-12-11"
-#define __GIT_DATE_UT__ 1733938191
+#define __GIT_DATE_UT__ 1733950949
